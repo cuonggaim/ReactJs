696a247d4d97494e9cae4d38bf2e9266f6e6d34b
CuongGaim <cuonggaim@gmail.com>
2022-04-09 08:01:22 +0700

    Hoàn thiện component chọn lịch kham benh

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -1,102 +1,115 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import HomeHeader from '../../HomePage/HomeHeader';
-import './DetailDoctor.scss';
-import { getDetailInfoDoctor } from '../../../services/userService';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import HomeHeader from "../../HomePage/HomeHeader";
+import "./DetailDoctor.scss";
+import { getDetailInfoDoctor } from "../../../services/userService";
 import { LANGUAGES } from "../../../utils";
-import DoctorSchedule from './DoctorSchedule';
+import DoctorSchedule from "./DoctorSchedule";
 
 class DetailDoctor extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            detailDoctor: {}
-        }
+            detailDoctor: {},
+            currentDoctorId: -1,
+        };
     }
 
     async componentDidMount() {
-        if (this.props.match && this.props.match.params && this.props.match.params.id) {
+        if (
+            this.props.match &&
+            this.props.match.params &&
+            this.props.match.params.id
+        ) {
             let id = this.props.match.params.id;
+            this.setState({
+                currentDoctorId: id,
+            });
             let res = await getDetailInfoDoctor(id);
             if (res && res.errCode === 0) {
                 this.setState({
-                    detailDoctor: res.data
-                })
+                    detailDoctor: res.data,
+                });
             }
         }
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot){
-
-    }
+    componentDidUpdate(prevProps, prevState, snapshot) {}
 
     render() {
         let { language } = this.props;
         let { detailDoctor } = this.state;
-        let nameVi = '', nameEn = '';
+        let nameVi = "",
+            nameEn = "";
         if (detailDoctor && detailDoctor.positionData) {
             nameVi = `${detailDoctor.positionData.valueVi}, ${detailDoctor.lastName} ${detailDoctor.firstName}`;
             nameEn = `${detailDoctor.positionData.valueEn}, ${detailDoctor.firstName} ${detailDoctor.lastName}`;
         }
         return (
             <>
-                <HomeHeader isShowBanner={false}/>
+                <HomeHeader isShowBanner={false} />
                 <div className="doctor-detail-container">
                     <div className="intro-doctor">
                         <div
                             className="content-left"
-                            style={{ backgroundImage: `url(${detailDoctor && detailDoctor.image ? detailDoctor.image: ''})`}}>
-                        </div>
+                            style={{
+                                backgroundImage: `url(${
+                                    detailDoctor && detailDoctor.image
+                                        ? detailDoctor.image
+                                        : ""
+                                })`,
+                            }}
+                        ></div>
                         <div className="content-right">
                             <div className="up">
                                 {language === LANGUAGES.VI ? nameVi : nameEn}
                             </div>
                             <div className="down">
-                                {detailDoctor && detailDoctor.Markdown && 
-                                detailDoctor.Markdown.description &&
-                                <span>
-                                    {detailDoctor.Markdown.description}
-                                </span>}
+                                {detailDoctor &&
+                                    detailDoctor.Markdown &&
+                                    detailDoctor.Markdown.description && (
+                                        <span>
+                                            {detailDoctor.Markdown.description}
+                                        </span>
+                                    )}
                             </div>
                         </div>
                     </div>
                     <div className="schedule-doctor">
                         <div className="content-left">
                             <DoctorSchedule
-                                doctorIdFromParent={detailDoctor && detailDoctor.id ? detailDoctor.id : -1}
+                                doctorIdFromParent={this.state.currentDoctorId}
                             />
                         </div>
-                        <div className="content-right">
-                            
-                        </div>
+                        <div className="content-right"></div>
                     </div>
                     <div className="detail-info-doctor">
-                        {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML &&
-                        <div dangerouslySetInnerHTML={{__html: detailDoctor.Markdown.contentHTML}}>
-                            
-                        </div>}
+                        {detailDoctor &&
+                            detailDoctor.Markdown &&
+                            detailDoctor.Markdown.contentHTML && (
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: detailDoctor.Markdown
+                                            .contentHTML,
+                                    }}
+                                ></div>
+                            )}
                     </div>
-                    <div className="comment-doctor">
-
-                    </div>
+                    <div className="comment-doctor"></div>
                 </div>
             </>
-        )
+        );
     }
-
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         language: state.app.language,
-    }
-}
+    };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
 
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(DetailDoctor)
+export default connect(mapStateToProps, mapDispatchToProps)(DetailDoctor);
--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './ManageSchedule.scss';
-import { FormattedMessage } from 'react-intl';
-import Select from 'react-select';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./ManageSchedule.scss";
+import { FormattedMessage } from "react-intl";
+import Select from "react-select";
 import * as actions from "../../../store/actions";
 import { CRUD_ACTIONS, LANGUAGES, dateFormat } from "../../../utils";
-import DatePicker from '../../../components/Input/DatePicker';
-import moment from 'moment';
-import { toast } from 'react-toastify';
-import _ from 'lodash';
-import { saveBulkScheduleDoctor } from '../../../services/userService';
+import DatePicker from "../../../components/Input/DatePicker";
+import moment from "moment";
+import { toast } from "react-toastify";
+import _ from "lodash";
+import { saveBulkScheduleDoctor } from "../../../services/userService";
 
 class ManageSchedule extends Component {
-
-    constructor(props) { 
+    constructor(props) {
         super(props);
         this.state = {
             listDoctors: [],
             selectedDoctor: {},
-            currentDate: '',
-            rangeTime: []
-        }
+            currentDate: "",
+            rangeTime: [],
+        };
     }
 
     componentDidMount() {
@@ -29,27 +28,27 @@ class ManageSchedule extends Component {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.allDoctors !== this.props.allDoctors){
+        if (prevProps.allDoctors !== this.props.allDoctors) {
             let dataSelect = this.buildDataInputSelect(this.props.allDoctors);
             this.setState({
-                listDoctors: dataSelect
-            })
+                listDoctors: dataSelect,
+            });
         }
-        if (prevProps.allScheduleTime !== this.props.allScheduleTime){
+        if (prevProps.allScheduleTime !== this.props.allScheduleTime) {
             let data = this.props.allScheduleTime;
-            if (data && data.length > 0){
-                data = data.map(item => ({ ...item, isSelected: false}))
+            if (data && data.length > 0) {
+                data = data.map((item) => ({ ...item, isSelected: false }));
             }
             this.setState({
-                rangeTime: data
-            })
+                rangeTime: data,
+            });
         }
     }
 
     buildDataInputSelect = (inputData) => {
         let result = [];
-        let {language} = this.props;
-        if(inputData && inputData.length > 0) {
+        let { language } = this.props;
+        if (inputData && inputData.length > 0) {
             inputData.map((item, index) => {
                 let object = {};
                 let labelVi = `${item.lastName} ${item.firstName}`;
@@ -57,37 +56,36 @@ class ManageSchedule extends Component {
                 object.label = language === LANGUAGES.EN ? labelEn : labelVi;
                 object.value = item.id;
                 result.push(object);
-            })
-            
+            });
         }
         return result;
-    }
+    };
 
     handleChangeSelect = async (selectedOption) => {
         this.setState({ selectedDoctor: selectedOption });
-    }
+    };
 
     handleOnChangeDatePicker = (date) => {
         this.setState({
-            currentDate: date[0]
-        })
-    }
+            currentDate: date[0],
+        });
+    };
 
     handleClickBtnTime = (time) => {
         let { rangeTime } = this.state;
         if (rangeTime && rangeTime.length > 0) {
-            rangeTime = rangeTime.map(item => {
+            rangeTime = rangeTime.map((item) => {
                 if (item.id === time.id) item.isSelected = !item.isSelected;
                 return item;
-            })
+            });
             this.setState({
-                rangeTime: rangeTime
-            })
+                rangeTime: rangeTime,
+            });
         }
-    }
+    };
 
     handleSaveSchedule = async () => {
-        let { rangeTime, selectedDoctor, currentDate  } = this.state;
+        let { rangeTime, selectedDoctor, currentDate } = this.state;
         let result = [];
 
         if (!currentDate) {
@@ -101,7 +99,9 @@ class ManageSchedule extends Component {
         let formatedDate = new Date(currentDate).getTime();
 
         if (rangeTime && rangeTime.length > 0) {
-            let selectedTime = rangeTime.filter(item => item.isSelected === true);
+            let selectedTime = rangeTime.filter(
+                (item) => item.isSelected === true
+            );
             if (selectedTime && selectedTime.length > 0) {
                 selectedTime.map((schedule, index) => {
                     let object = {};
@@ -109,7 +109,7 @@ class ManageSchedule extends Component {
                     object.date = formatedDate;
                     object.timeType = schedule.keyMap;
                     result.push(object);
-                })
+                });
             } else {
                 toast.error("Invalid selected time");
                 return;
@@ -119,26 +119,32 @@ class ManageSchedule extends Component {
         let res = await saveBulkScheduleDoctor({
             arrSchedule: result,
             doctorId: selectedDoctor.value,
-            formatedDate: formatedDate
-        })
-        console.log(res)
-    }
+            formatedDate: formatedDate,
+        });
+        if (res && res.errCode === 0) {
+            toast.success("Save info success");
+        } else {
+            toast.error("Error saveBulkScheduleDoctor");
+        }
+    };
 
     render() {
-
         let { rangeTime } = this.state;
         let { language } = this.props;
+        let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
 
         return (
             <React.Fragment>
                 <div className="manage-schedule-container">
                     <div className="m-s-title">
-                        <FormattedMessage id="manage-schedule.title"/>
+                        <FormattedMessage id="manage-schedule.title" />
                     </div>
                     <div className="container">
                         <div className="row">
                             <div className="col-6 form-group">
-                                <label><FormattedMessage id="manage-schedule.choose-doctor"/></label>
+                                <label>
+                                    <FormattedMessage id="manage-schedule.choose-doctor" />
+                                </label>
                                 <Select
                                     value={this.state.selectedDoctor}
                                     onChange={this.handleChangeSelect}
@@ -146,59 +152,71 @@ class ManageSchedule extends Component {
                                 />
                             </div>
                             <div className="col-6 form-group">
-                                <label><FormattedMessage id="manage-schedule.choose-date"/></label>
+                                <label>
+                                    <FormattedMessage id="manage-schedule.choose-date" />
+                                </label>
                                 <DatePicker
                                     onChange={this.handleOnChangeDatePicker}
                                     className="form-control"
                                     value={this.state.currentDate}
-                                    minDate={new Date()}
+                                    minDate={yesterday}
                                 />
                             </div>
                             <div className="col-12 pick-hour-container">
-                                {rangeTime && rangeTime.length > 0 &&
+                                {rangeTime &&
+                                    rangeTime.length > 0 &&
                                     rangeTime.map((item, index) => {
                                         return (
-                                            <button 
-                                                className={item.isSelected === true ? "btn btn-schedule active" : "btn btn-schedule"}
+                                            <button
+                                                className={
+                                                    item.isSelected === true
+                                                        ? "btn btn-schedule active"
+                                                        : "btn btn-schedule"
+                                                }
                                                 key={index}
-                                                onClick={() => this.handleClickBtnTime(item)}
+                                                onClick={() =>
+                                                    this.handleClickBtnTime(
+                                                        item
+                                                    )
+                                                }
                                             >
-                                                {language === LANGUAGES.EN ? item.valueEn : item.valueVi}
+                                                {language === LANGUAGES.EN
+                                                    ? item.valueEn
+                                                    : item.valueVi}
                                             </button>
-                                        )
-                                    })
-                                }
+                                        );
+                                    })}
                             </div>
                             <div className="col-12">
-                                <button 
+                                <button
                                     className="btn btn-primary btn-save-schedule"
                                     onClick={() => this.handleSaveSchedule()}
                                 >
-                                    <FormattedMessage id="manage-schedule.save"/>
+                                    <FormattedMessage id="manage-schedule.save" />
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </React.Fragment>
-        )
+        );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
         allDoctors: state.admin.allDoctors,
         allScheduleTime: state.admin.allScheduleTime,
-    }
-}
+    };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
     return {
         fetchAllDoctors: () => dispatch(actions.fetchAllDoctors()),
         fetchAllScheduleTime: () => dispatch(actions.fetchAllScheduleTime()),
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(ManageSchedule)
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSchedule);
